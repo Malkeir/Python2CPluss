3bde76d5f4e58c5dce6c6b84dcb5115a39a889a0
malkeir <badorr@charter.net>
2021-06-25 13:20:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import os
+
+
+def CppProjectFolder(pyDir,cppDir):
+    check to see if cppDir exists
+        for file in pyDir
+            pyFile = OpenPython(file)
+
+    listOfFilesNeeded=[]
+    OpenPython(pyDir)
+    return listOfFilesNeeded
+
+def CreateCPPFile(cppDir):
+    print('create cpp file based on the name that you specified and mantains its location')
+    return open(cppDir, w+)
+
+def WritingToCPPFile():
+
+    print('passed in cpp file and writing to it')
+def WordMatching():
+    listOfCombinations =[]
+    dictionMatching = {}
+    if word is in diction
+    print("finding keywords for syntax")
+
+        listofCombinations.append( dictionMatching.get())
+
+    WritingToCPPFile()
+
+def OpenPython(pyDir):
+
+    print("opened python file")
+    for file in Dir:
+        if lines end in .py
+            WordMatching();
+
+
+
+def Main():
+    cPPDir = input('where do you want to write the cpp file')
+    pyDir = input('what python project would you like to convert please give dir?:')
+    cppFile = CreateCPPFile(cPPDir)
+    pythonFile = OpenPython(pyDir)
+    +\rint('hello work')
+Main()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
